fada5f48978dad675aa397760ae6afb659a5ee9a
ssmith-wombatweb <ssmith@wombatweb.us>
2017-02-15 19:36:16 -0800

    Refactored code to be more effecient. Added a check for fs.appendFile calls and confirms that messages are formatted correctly.

--- test/_log.js
+++ test/_log.js
@@ -2,150 +2,135 @@ const rewire = require('rewire');
 const expect = require('chai').expect;
 const sinon = require('sinon');
 
-const log = rewire('../utils/log');
+const log = rewire('../log');
+
+function compareLogMsg(sent, expected) {
+  const logStrings = [];
+
+  sent.forEach((args) => {
+    logStrings.push(args[1].substring(args[1].indexOf('\t') + 1));
+  });
+
+  Object.keys(expected).forEach((key, index) => {
+    const msg = `${expected[key].ip}\t${expected[key].method}\t${expected[key].url}\t\t${expected[key].level}\t${expected[key].logMsg}\n`;
+
+    const resultMsg = logStrings[index];
+
+    expect(msg).to.equal(resultMsg);
+  });
+}
 
 describe('Debug Logging Utility', () => {
+  beforeEach(() => {
+    this.console = {
+      log: sinon.spy(),
+    };
+
+    this.fs = {
+      stat: sinon.spy(),
+      appendFile: sinon.spy(),
+    };
+
+    log.__set__('fs.stat', this.fs.stat);
+    log.__set__('fs.appendFile', this.fs.appendFile);
+    log.__set__('console', this.console);
+    log.__set__('process.env.DEBUG', 'true');
+  });
+
+  afterEach(() => {
+    this.fs.stat.reset();
+    this.fs.appendFile.reset();
+  });
   describe('Testing Each Log Level', () => {
-    beforeEach(() => {
-      this.console = {
-        log: sinon.spy(),
-      };
-      this.fs = {
-        stat: sinon.spy(),
-      };
-
-      log.__set__('fs.stat', this.fs.stat);
-      log.__set__('console', this.console);
-      log.__set__('process.env.DEBUG', 'true');
-    });
-
-    afterEach(() => {
-      this.fs.stat.reset();
-    });
-
     describe('Log Level Set to true.', () => {
+      beforeEach(() => {
+        this.logLvlMessages = {
+          info: {
+            logMsg: 'Sent an INFO log message to the console.',
+            method: 'METHOD',
+            url: 'URL',
+            ip: 'IP ADDRESS',
+            level: 'INFO',
+          },
+          debug: {
+            logMsg: 'Sent a DEBUG log message to the console.',
+            method: 'METHOD',
+            url: 'URL',
+            ip: 'IP ADDRESS',
+            level: 'DEBUG',
+          },
+          error: {
+            logMsg: 'Sent a ERROR log message to the console.',
+            method: 'METHOD',
+            url: 'URL',
+            ip: 'IP ADDRESS',
+            level: 'ERROR',
+          },
+        };
+      });
+
       it('Log All Level Messages to the Console.', () => {
-        const _this = this;
+        const t = this;
 
-        log.debug({
-          logMsg: 'Sent an INFO log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'INFO',
-        });
+        log.debug(t.logLvlMessages.info);
 
-        log.debug({
-          logMsg: 'Sent a DEBUG log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'DEBUG',
-        });
+        log.debug(t.logLvlMessages.debug);
 
-        log.debug({
-          logMsg: 'Sent a ERROR log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'ERROR',
-        });
+        log.debug(t.logLvlMessages.error);
 
-        expect(_this.console.log.callCount).to.equal(3);
-        expect(_this.fs.stat.callCount).to.equal(3);
+        compareLogMsg(t.fs.appendFile.args, this.logLvlMessages, expect);
+
+        expect(t.console.log.callCount).to.equal(3);
+        expect(t.fs.stat.callCount).to.equal(3);
+        expect(t.fs.appendFile.callCount).to.equal(3);
       });
     });
     describe('Log Level Set to debug.', () => {
       beforeEach(() => {
-        this.console = {
-          log: sinon.spy(),
-        };
-
-        log.__set__('console', this.console);
         log.__set__('process.env.DEBUG', 'debug');
       });
       it('Log All Level Messages to the Console.', () => {
-        const _this = this;
+        const t = this;
 
-        log.debug({
-          logMsg: 'Sent an INFO log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'INFO',
-        });
+        log.debug(t.logLvlMessages.info);
 
-        log.debug({
-          logMsg: 'Sent a DEBUG log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'DEBUG',
-        });
+        log.debug(t.logLvlMessages.debug);
 
-        log.debug({
-          logMsg: 'Sent a ERROR log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'ERROR',
-        });
+        log.debug(t.logLvlMessages.error);
 
-        expect(_this.console.log.callCount).to.equal(3);
-        expect(_this.fs.stat.callCount).to.equal(3);
+        compareLogMsg(t.fs.appendFile.args, this.logLvlMessages, expect);
+
+        expect(t.console.log.callCount).to.equal(3);
+        expect(t.fs.stat.callCount).to.equal(3);
+        expect(t.fs.appendFile.callCount).to.equal(3);
       });
     });
     describe('Log Level Set to info.', () => {
       beforeEach(() => {
-        this.console = {
-          log: sinon.spy(),
-        };
-
-        log.__set__('console', this.console);
         log.__set__('process.env.DEBUG', 'info');
       });
       it('Log Only INFO and ERROR level messages to Console.', () => {
-        const _this = this;
+        const t = this;
 
-        log.debug({
-          logMsg: 'Sent an INFO log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'INFO',
-        });
+        log.debug(t.logLvlMessages.info);
 
-        log.debug({
-          logMsg: 'Sent a DEBUG log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'DEBUG',
-        });
+        log.debug(t.logLvlMessages.debug);
 
-        log.debug({
-          logMsg: 'Sent a ERROR log message to the console.',
-          method: 'METHOD',
-          url: 'URL',
-          ip: 'IP ADDRESS',
-          level: 'ERROR',
-        });
+        log.debug(t.logLvlMessages.error);
 
-        expect(_this.console.log.callCount).to.equal(2);
-        expect(_this.fs.stat.callCount).to.equal(3);
+        compareLogMsg(t.fs.appendFile.args, this.logLvlMessages, expect);
+
+        expect(t.console.log.callCount).to.equal(2);
+        expect(t.fs.stat.callCount).to.equal(3);
+        expect(t.fs.appendFile.callCount).to.equal(3);
       });
     });
     describe('Log Level Set to error.', () => {
       beforeEach(() => {
-        this.console = {
-          log: sinon.spy(),
-        };
-
-        log.__set__('console', this.console);
         log.__set__('process.env.DEBUG', 'error');
       });
       it('Log Only ERROR level messages to Console.', () => {
-        const _this = this;
+        const t = this;
 
         log.debug({
           logMsg: 'Sent an INFO log message to the console.',
@@ -171,11 +156,26 @@ describe('Debug Logging Utility', () => {
           level: 'ERROR',
         });
 
-        expect(_this.console.log.callCount).to.equal(1);
-        expect(_this.fs.stat.callCount).to.equal(3);
+        compareLogMsg(t.fs.appendFile.args, this.logLvlMessages, expect);
+
+        expect(t.console.log.callCount).to.equal(1);
+        expect(t.fs.stat.callCount).to.equal(3);
+        expect(t.fs.appendFile.callCount).to.equal(3);
       });
     });
   });
-  // describe('Testing Formatting Variations.')
+  describe('Testing Empty Field Handling', () => {
+    it('Replace undefined with spaces.', () => {
+      const t = this;
+
+      log.debug({
+        logMsg: 'Testing undefined fields.',
+        level: 'INFO',
+      });
+
+      expect(t.console.log.callCount).to.equal(1);
+      expect(t.fs.stat.callCount).to.equal(1);
+    });
+  });
 });
 
