60cb01dd6fe7c73040ee38f491c91ff537b048c5
Dan Cobb <dancobb@gmail.com>
2017-02-20 14:10:59 -0500

    Switch statement and start of auto version update

--- updater.js
+++ updater.js
@@ -1,10 +1,58 @@
 const fs = require('fs');
 
 class versionUpdate {
-  update(ver, rel) {
+  updateManual(ver, rel) {
     this.current = ver.split('.');
     this.oldPackage = JSON.parse(fs.readFileSync('package.json'));
-    return this.current;
+    this.n = '';
+    switch (rel) {
+      case 'patch':
+        this.n = parseInt(this.current[2], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.current[1]}.${this.n}`;
+        break;
+      case 'minor':
+        this.n = parseInt(this.current[1], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.n}.${this.current[2]}`;
+        break;
+      case 'major':
+        this.n = parseInt(this.current[0], 10) + 1;
+        this.updateVersion = `${this.n}.0.0`;
+        break;
+      case 'test':
+        this.n = parseInt(this.current[0], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.current[1]}.${this.current[2]}`;
+        break;
+      default:
+        return 'Invalid release type.';
+    }
+    return this.updateVersion;
+  }
+  updateAuto(rel) {
+    this.oldPackage = JSON.parse(fs.readFileSync('package.json'));
+    console.log(this.oldPackage.version)
+    this.current = this.oldPackage.version.split('.');
+    this.n = '';
+    switch (rel) {
+      case 'patch':
+        this.n = parseInt(this.current[2], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.current[1]}.${this.n}`;
+        break;
+      case 'minor':
+        this.n = parseInt(this.current[1], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.n}.${this.current[2]}`;
+        break;
+      case 'major':
+        this.n = parseInt(this.current[0], 10) + 1;
+        this.updateVersion = `${this.n}.0.0`;
+        break;
+      case 'test':
+        this.n = parseInt(this.current[0], 10) + 1;
+        this.updateVersion = `${this.current[0]}.${this.current[1]}.${this.current[2]}`;
+        break;
+      default:
+        return 'Invalid release type.';
+    }
+    return this.updateVersion;
   }
   updatePackage(ver) {
     this.oldPackage = JSON.parse(fs.readFileSync('package.json'));
@@ -12,4 +60,4 @@ class versionUpdate {
   }
 }
 const versionUpdater = new versionUpdate();
-console.log(versionUpdater.update('1.0.0', 'minor'));
+console.log(versionUpdater.updateAuto('test'));
